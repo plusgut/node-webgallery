e87d4ae9a422c93f1252399dce943680fe40ecff
Carlo Jeske <c.jeske@blooparksystems.de>
2012-11-01 13:14:53 +0100

    Added complete structure for serving needed data (json-content for galleries and pictures, plus delivering the binary of the picture)

--- app.js
+++ app.js
@@ -1,12 +1,57 @@
 #!/usr/bin/env node
 var express = require('express');
-
+var fs = require('fs');
 var app = express();
+app.use(express.static(__dirname + '/public'));
 
 app.listen(7353);
 
 app.get('/' , function(req, res) {
-  console.log("blub")
-  res.end("blarg")
+  res.sendfile('views/index.html');
 });
 
+app.get('/pics', function(req, res){
+  var path = __dirname + '/pics';
+  fs.readdir(path, function(err, files){
+    if(err){
+      res.send(500);
+    } else{
+       var newFiles = [];
+       for(var fileIndex in files){
+         if(files.hasOwnProperty(fileIndex) && files[fileIndex][0] != '.'){
+           newFiles.push(files[fileIndex]);
+         }
+       }
+       res.end(JSON.stringify(newFiles));
+    }
+  });
+})
+
+app.get('/pics/:galleryName', function(req, res){
+  var path = __dirname + '/pics/' + req.params.galleryName 
+  fs.exists(path, function(exists){
+    if(exists){
+      fs.readdir(path, function(err, files){
+        if(err){
+          res.send(500);
+        } else{
+          res.end(JSON.stringify(files));
+        }
+      })
+    } else{
+      res.send(404);
+    }
+  });
+});
+
+app.get('/pics/:galleryName/:pictureName', function(req, res){
+  var path = __dirname + '/pics/' + req.params.galleryName + '/' + req.params.pictureName;
+  fs.exists(path, function(exists){
+    if(exists){
+      res.sendfile(path);
+
+    } else{
+      res.send(404);
+    }
+  });
+})
